a8d71a446bcc0cacb56f0bf143a44ed3378585d9
nehoraiharush <101175556+nehoraiharush@users.noreply.github.com>
2022-04-12 19:07:59 +0300

    adding the details button 12/4/2022

--- controllers/characters.js
+++ controllers/characters.js
@@ -3,20 +3,20 @@ import fetch from 'node-fetch';
 const router = express.Router();
 
 
-router.get('/getCharacter',async(req,res) => {
+router.get('/getCharacter', async (req, res) => {
     const url = 'https://www.breakingbadapi.com/api/characters';
-    const data_response = await fetch(url,{method:'get'});
+    const data_response = await fetch(url, { method: 'get' });
     const data = await data_response.json();
-    res.render('characters',{
-        data:data
+    res.render('characters', {
+        data: data
     })
 })
 
-router.get('/quotes',async(req,res) => {
+router.get('/quotes', async (req, res) => {
     const url = 'https://www.breakingbadapi.com/api/quotes';
     const url2 = 'https://www.breakingbadapi.com/api/characters';
-    const data_response = await fetch(url,{method:'get'});
-    const data_response2 = await fetch(url2,{method:'get'});
+    const data_response = await fetch(url, { method: 'get' });
+    const data_response2 = await fetch(url2, { method: 'get' });
     const data = await data_response.json();
     const data2 = await data_response2.json();
 
@@ -25,25 +25,35 @@ router.get('/quotes',async(req,res) => {
     data.forEach(quote => {
         let the_player = {};
         data2.forEach(person => {
-            if(person.name == quote.author){
+            if (person.name == quote.author) {
                 the_player = person
             }
         })
-        let obj = { the_quote: quote, player: the_player  };
+        let obj = { the_quote: quote, player: the_player };
         formatted_data.push(obj);
     });
 
-    res.render('quotes',{
-        data:formatted_data
+    res.render('quotes', {
+        data: formatted_data
     })
 })
 
-router.get('/episodes',async(req,res) => {
+router.get('/episodes', async (req, res) => {
     const url = 'https://www.breakingbadapi.com/api/episodes';
-    const data_response = await fetch(url,{method:'get'});
+    const data_response = await fetch(url, { method: 'get' });
     const data = await data_response.json();
-    res.render('episodes',{
-        data:data
+    res.render('episodes', {
+        data: data
+    })
+})
+
+router.get('/:character_id', async (req, res) => {
+    const character_id = req.params.character_id;
+    const url = `https://breakingbadapi.com/api/characters/` + character_id;
+    const data_response = await fetch(url, { method: 'get' });
+    const data = await data_response.json();
+    return res.render('EachCharacter', {
+        personById: data,
     })
 })
 
